8fbad47fc4f5cb97b47d29eb9b27365437ea0501
JoJo Myles <joneshiamyles31@gmail.com>
2021-05-17 09:13:06 -0400

    intial commit

---
+++ src/java2_prac/Runner.java
@@ -0,0 +1,18 @@
+package java2_prac;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		String aWord = "Hello JoJo";
+		System.out.println(aWord);
+		
+		int num = 2;
+		int num2 = 4;
+		
+		System.out.println(num);
+		
+		
+
+	}
+
+}
